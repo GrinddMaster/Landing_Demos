f154ab24aaf74bedb284bb71de93c842c13f8e77
GrinddMaster <Ali.Swelam@student.aast.edu>
2025-12-10 00:29:55 +0200

    Refernce the Tour Component by sending the sdk key and the spaceID by passing it to the Compnent declare your env variables and pass them to the component

--- src/Components/TourComponents/Tour.tsx
+++ src/Components/TourComponents/Tour.tsx
@@ -2,18 +2,22 @@ import { useEffect, useRef } from 'react';
 import { setupSdk } from '@matterport/sdk';
 import sdkInterface from '@/sdk_interface';
 
-export default function TourViewContainer() {
+//  use this import.meta.env.VARIABLE to get your .env data;
+
+export default function TourViewContainer(
+  spaceId:string,
+  sdkKey:string,
+  tourUrl:string =`https://my.matterport.com/show/?m=${spaceId}&applicationKey=${sdkKey}` ) 
+  {
   const iframeRef = useRef(null);
   const started = useRef(false);
-  const spaceId = import.meta.env.VITE_SPACE_ID;
-  const sdkKey = import.meta.env.VITE_SDK_KEY;
   const port = import.meta.env.VITE_PORT;
 
   useEffect(() => {
     if (started.current) return;
     started.current = true;
 
-    const sdk_options = {
+    const sdk_options:object = {
       iframe: iframeRef.current,
       space: spaceId,
       domain: `localhost${port}`,
@@ -34,7 +38,7 @@ export default function TourViewContainer() {
       ref={iframeRef}
       title="Matterport Showcase"
       style={{ width: '100%', height: '600px', border: 0 }}
-      src={`https://my.matterport.com/show/?m=${spaceId}&applicationKey=${sdkKey}`}
+      src={tourUrl}
       allow="autoplay; fullscreen; web-share; xr-spatial-tracking"
     />
   );
