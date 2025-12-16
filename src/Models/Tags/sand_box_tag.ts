50c940216227b740bf9f2af798a1b75574382743
GrinddMaster <Ali.Swelam@student.aast.edu>
2025-12-16 20:28:22 +0200

    Removed the static tag

--- src/Models/Tags/sand_box_tag.ts
+++ src/Models/Tags/sand_box_tag.ts
@@ -4,25 +4,10 @@ import { CommonMpSdk } from '@matterport/sdk/sdk';
 
 
 async function setupSandBox(mpSdk : CommonMpSdk) {
-  const [tagId] = await mpSdk.Tag.add({
-    label: 'بطنيات',
-    color: {r:1,g:1,b:1},
-    description: 'تعالى اشتري أحلى بطنية من عندنا يا عامنا',
-    anchorPosition: {
-      x: 3.5046774605353463,
-      y: 1.473522837205481,
-      z: -1.840956315987719,
-    },
-    stemVector: {
-      x: 0.4003924002376688,
-      y: -0.03233699978063469,
-      z: 0.915773030983718,
-    },
-  });
 
   const [sandboxid, messenger] = await mpSdk.Tag.registerSandbox(itemHtml);
   
-  await mpSdk.Tag.attach(tagId, sandboxid);
+  await mpSdk.Tag.attach('tU7wFgb78rQ', sandboxid);
   messenger.on('addToCart', (itemData) => {
     console.log('Received item from sandbox', itemData);
     addCartItem(itemData);
