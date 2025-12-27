ea9a90bb2ce83fd91d8cc00631cb0ea2039cc1bb
GrinddMaster <Ali.Swelam@student.aast.edu>
2025-12-27 20:17:20 +0200

    The chatbot view view is working but still its implementation isn't this should work later

---
+++ src/Views/ChatView/chat-view.tsx
@@ -0,0 +1,21 @@
+import ChatBot from '@/Components/ChatComponents/ChatBot';
+import TourView from '@/Components/TourComponents/Tour';
+export default function cartView() {
+  const key = import.meta.env.VITE_SDK_KEY;
+  const Id = import.meta.env.VITE_SPACE_ID;
+  return (
+    <section>
+      <div className="flex flex-col items-center">
+        <h2 className="bg-linear-to-r from-violet-500 via-blue-500 to-green-500 bg-clip-text py-4 text-4xl font-extrabold tracking-wide text-transparent drop-shadow-lg">
+            ChatBot
+        </h2>
+      </div>
+      <div className="flex items-center pr-12 pl-12">
+        <TourView sdkKey={key} spaceId={Id} />
+      </div>
+      <div className="flex flex-col items-center pt-9">
+        <ChatBot />
+      </div>
+    </section>
+  );
+}
--- src/Views/home-page.tsx
+++ src/Views/home-page.tsx
@@ -1,14 +1,13 @@
 import Header from '@/Components/header';
 import TitleView from '@/Components/title';
 import CartView from './CartView/cart-view';
+import ChatView from './ChatView/chat-view';
 import ReservationView from './ReservationView/reservation-view';
 export default function homePage() {
   return (
     <div>
       <Header />
-      <TitleView />
-      <CartView/>
-      <ReservationView/>
+      <ChatView/>
     </div>
   );
 }
